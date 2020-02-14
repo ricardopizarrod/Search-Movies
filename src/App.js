4a61fe5e9c4ba76fd24543a93171601f74574d0f
Ricardo Pizarro <ricardopizarro@MacBook-Pro.local>
2020-02-14 16:43:44 -0300

    Se agrega el componente SearchForm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { Title } from "./componets/Title";
-import logo from './logo.svg';
+import { SearchForm } from "./componets/SearchForm";
 import './App.css';
 import 'bulma/css/bulma.css'
 
@@ -8,9 +8,12 @@ class App extends Component{
   render() {
     return (
       <div className="App">
-        <Title>Search Movies</Title>
+        <Title>Buscar películas</Title>
+        <div className="SearchForm-wrapper">
+         <SearchForm/>
+        </div>
       </div>
-    ); 
+    );  
   }
 }
 
